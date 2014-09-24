68750867696bb176bb9f7ed7a5a327618592703a
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2014-09-24 17:46:30 -0400

    Prints numbers.

--- forward_euler.c
+++ forward_euler.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
+    int i;
+    for(i=0; i< 100; i++)
+    {
+        printf("%lg %lg\n", (double)i, sin(i));
+    }
+
     printf("Hello!\n");
 
     return 0;
